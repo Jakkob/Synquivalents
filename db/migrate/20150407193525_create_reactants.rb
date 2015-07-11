9a572a2b779bc2d81ce365cd295ddc782fef75cf
Jakkob <bluemoomba@gmail.com>
2015-07-11 15:32:29 -0400

    Initial update of migration files

--- db/migrate/20150407193525_create_reactants.rb
+++ db/migrate/20150407193525_create_reactants.rb
@@ -1,7 +1,7 @@
 class CreateReactants < ActiveRecord::Migration
   def change
     create_table :reactants do |t|
-      t.boolean :starting_material
+      t.boolean :starting_material, null: false, default: false
       t.float :equivalents
       t.references :entry, index: true, foreign_key: true
       t.references :reagents, index: true, foreign_key: true
--- db/migrate/20150413125329_add_default_value_to_reactants.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultValueToReactants < ActiveRecord::Migration
-  def change
-  	change_column_default :reactants, :starting_material, false
-  end
-end
