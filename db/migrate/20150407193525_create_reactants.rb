0f7b5c1ca18a001d4c51395833524b5adecddce8
Jakkob <bluemoomba@gmail.com>
2015-07-11 15:49:38 -0400

    Migration repair for Postgres

--- db/migrate/20150407193525_create_reactants.rb
+++ db/migrate/20150407193525_create_reactants.rb
@@ -4,7 +4,7 @@ class CreateReactants < ActiveRecord::Migration
       t.boolean :starting_material, null: false, default: false
       t.float :equivalents
       t.references :entry, index: true, foreign_key: true
-      t.references :reagents, index: true, foreign_key: true
+      t.references :reagent, index: true, foreign_key: true
 
       t.timestamps null: false
     end
